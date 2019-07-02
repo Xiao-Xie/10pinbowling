f8023aa8b508a6f21995c279938c9fdafa69385c
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-02 13:30:12 -0400

    express serves static files

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <p>This is rendering from react</p>;
+  }
+}
+
+export default App;
